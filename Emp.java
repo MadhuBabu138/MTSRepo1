03d38904a0919ac4b12821b5a8f522fa732d227d
Madhu.K <madhukallaclang@gmail.com>
2024-10-03 21:03:05 +0530

    commit2 Emp class with instance method display

--- Emp.java
+++ Emp.java
@@ -3,4 +3,10 @@ public class Emp
     int eno;
     String ename;
     float sal;
+    void display()
+    {
+        System.out.println("eno:\t"+eno);
+        System.out.println("ename:\t"+ename);
+        System.out.println("sal:\t"+sal);
+    }
 }
